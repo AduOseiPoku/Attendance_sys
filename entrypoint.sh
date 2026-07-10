df932ab1297b968b9e085d5e385bd02ebd814c05
AduPoku <adupokuosei1234@gmail.com>
2026-07-10 11:38:57 +0000

    gunicorn fix again

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,4 +19,4 @@ python manage.py collectstatic --noinput
 
 # Finally start the app
 echo "Starting Gunicorn..."
-exec gunicorn core.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+exec gunicorn core.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2 --worker-tmp-dir /tmp --log-file -
